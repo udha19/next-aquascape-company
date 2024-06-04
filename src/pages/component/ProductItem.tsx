1ea41427a2ff0dffa1a5b0336f694bcccc965aed
x <x@eks.eks>
2024-06-04 15:44:34 +0700

    add nullish oprt for build

--- src/pages/component/ProductItem.tsx
+++ src/pages/component/ProductItem.tsx
@@ -29,14 +29,14 @@ export default function ProductItem(product: any) {
           alt="Card background"
           className="object-cover rounded-xl"
           src={
-            product?.product.image
-              ? product?.product.image
+            product?.product?.image
+              ? product?.product?.image
               : "https://static5.depositphotos.com/1032932/423/i/950/depositphotos_4231002-stock-photo-studio-shot-of-a-fish.jpg"
           }
           width={270}
         />
         <p className="font-bold text-large text-center mx-auto p-2 overflow-hidden text-ellipsis truncate">
-          {product?.product.name ? product?.product.name : "Our Product"}
+          {product?.product?.name ? product?.product?.name : "Our Product"}
         </p>
       </div>
       <Modal size="md" isOpen={isOpen} onClose={onClose}>
